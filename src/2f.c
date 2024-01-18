fefc526868c6f534e8d33dc34594a675312995c3
kaka27022 <117757580+kaka27022@users.noreply.github.com>
2024-01-18 19:22:32 -0300

    Add files via upload

--- src/2f.c
+++ src/2f.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "registro.h"
+
 #include "lista.h"
+#include "registro.h"
 #include "heapSort.h"
 
 // Gera arquivo temporario das n primeiras linhas do arquivo principal
@@ -117,80 +118,12 @@ void gerarblocos(int n, FILE *temp, FILE **fitas, int elementos[DOBRO], lista bl
 
 void intercalablocos (int n, FILE *temp, FILE **fitas, int elementos[DOBRO], lista blocos[DOBRO]){
 
-    Registro memoria[FITAS];
-    int from[FITAS];
-    short ativas[FITAS];
-    short saltofitas = 1;
-    int fita;
-    for(int i = 0; i < DOBRO; i++){
-        rewind(fitas[i]);
-    }
+    // Elementos conta a quantidade de elementos por fita
+    // Temp, onde sera escrito o resultado final
+    // Tbm usa um lista de blocos para intercalacao
+    // n é a quantidade de linhas
+    // 
 
-    // f = -1 se nao tiver nenhuma fita preenchida
-    int f = umaFitaPreenchida(elementos, FITAS, FITAS*saltofitas);
-    while(f == -1) {
-        fita = FITAS*saltofitas;
-        int bloco = 0;
-        while (!todasFitasPercorridas(fitas, elementos, FITAS, FITAS*(!saltofitas))){ // Nao percorreu todas as fitas
-
-            listaAdd(&blocos[fita], ftell(fitas[fita])/sizeof(Registro));
-            int limiteblocos[FITAS]; // termina o bloco de cada fita
-            for(int i = 0; i < FITAS; i++){
-                if(listaTam(&blocos[(FITAS*!saltofitas)+i])>bloco+1)
-                    limiteblocos[i] = listaPosicao(&blocos[(FITAS*!saltofitas)+i], bloco+1);
-                else
-                    limiteblocos[i] = elementos[(FITAS*!saltofitas)+i];
-            }
-
-            // Primeiro elemento de cada fita de entrada
-            for(int i = 0; i < FITAS; i++){
-                if(ftell(fitas[(FITAS*!saltofitas)+i])/sizeof(Registro) < (unsigned) limiteblocos[i]) {  // Tem elemento no bloco?
-                    fread(&memoria[i], sizeof(Registro), 1, fitas[(FITAS*!saltofitas)+i]); // Pega primeiro elemento
-                    if(ftell(fitas[(FITAS*!saltofitas)+i])/sizeof(Registro) >= (unsigned) limiteblocos[i]) // Foi o ultimo elemento?
-                        ativas[i] = 0;
-                    else 
-                        ativas[i] = 1;
-                }
-                else
-                    ativas[i] = -1;
-                from[i] = i+(!saltofitas*FITAS);
-            }
-
-            while(!blocosEsgotados(ativas, FITAS)){
-                // heapSortM(memoria, ativas, from, FITAS, 0)
-                fwrite(&memoria[0], sizeof(Registro), 1, fitas[fita]); // Menos elemento na fita alvo
-                elementos[fita]++; // aumenta o tamanho da fita
-                if(ativas[from[0]%FITAS]){ // se a fita do menor esta ativa
-                    fread(&memoria[0], sizeof(Registro), 1, fitas[from[0]]); // pega o primeiro elemento
-                    if(ftell(fitas[from[0]])/sizeof(Registro) == (unsigned) limiteblocos[from[0]%FITAS])
-                        ativas[from[0]%FITAS] = 0; //fita inativa
-                } else {
-                    ativas[from[0]%FITAS] = -1; 
-                }
-            }
-            fita = (fita%FITAS+1)%FITAS + FITAS*saltofitas; // proxima fita alvo
-            bloco++;
-        } 
-
-        for(int i = 0; i < FITAS; i++){
-            rewind(fitas[FITAS*saltofitas + i]);
-            rewind(fitas[FITAS*(!saltofitas) + i]);
-            elementos[FITAS * (!saltofitas) + i] = 0;
-            limparList(&blocos[FITAS*(!saltofitas) + i]);
-        }
-
-        saltofitas = !saltofitas; // troca as fitas de entrada com as fitas de saida
-        f = umaFitaPreenchida(elementos, FITAS, FITAS*(!saltofitas)); // resta apenas uma fita com elemento?
-    }
-
-    rewind(fitas[f]);
-    rewind(temp);
-    Registro r;
-    for(int i = 0; i < n; i++){
-        // joga elementos da fita f na fita principal
-        fread(&r, sizeof(Registro), 1, fitas[f]);
-        fwrite(&r, sizeof(Registro), 1, temp);
-    }
 }
 
 int main() {
@@ -222,8 +155,9 @@ int main() {
     for(int i = 0; i < DOBRO; i++)
         blocos[i].primeiro = NULL;
 
+    // Gera Arquivos de fitas com os valores em ordem, de acordo com os blocos
     gerarblocos(quantidade, temp, fitas, elementos, blocos);
-    intercalablocos(quantidade, temp, fitas, elementos, blocos);
+    // intercalablocos(quantidade, temp, fitas, elementos, blocos);
 
     for(int i = 0; i < DOBRO; i++)
         fclose(fitas[i]);
