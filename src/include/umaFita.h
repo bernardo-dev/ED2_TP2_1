d98d4c12957d0b51407d7b2923a3498e8a3b4745
Marcos Euzébio <markeuzebio@gmail.com>
2024-01-24 23:53:52 -0300

    Insercao do metodo F + 1 fitas

---
+++ src/include/umaFita.h
@@ -0,0 +1,12 @@
+#ifndef _F1_FITAS_
+#define _F1_FITAS_
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "registro.h"
+#include "util.h"
+
+FILE* f1fitas(FILE *arquivo_binario, unsigned int qtde_registros, Metrica*);
+
+#endif
